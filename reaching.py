6b516523d7188c8935721a45794fad65403f6052
Zinfidel <yamkoo@gmail.com>
2014-12-11 18:59:02 -0700

    Cleaned up code before moving on to RDs.

--- reaching.py
+++ reaching.py
@@ -35,35 +35,70 @@ def getGenSets(cfg):
     one variable. Also, the label number in the tuple will always be the key.
 
     :param AGraph cfg: The CFG for which to generate the gen set.
+    :rtype: dict[int, set[(str, str)]]
+    """
+
+    # Get generators for the various attributes for convenience.
+    # TODO: Remove str() calls after debugging is finished!
+    nodes = cfg.nodes()
+    labels = (str(node.name) for node in nodes)
+    variables = (str(node.attr["var"]) for node in nodes)
+    types = (str(node.attr["type"]) for node in nodes)
+
+    # Generate the genSet. For non assignment nodes, the value an empty set.
+    genSets = dict()
+    for node, label, variable, type in zip(nodes, labels, variables, types):
+        genSets[label] = {(variable, label)} if type == NODE_ASSN else set()
+
+    return genSets
+
+
+def getKillSets(genSets):
+    """
+    Calculates a kill set for control-flow graph, given its gen set.
+
+    The kill set is a dictionary with keys that map to the cfg's node labels.
+    The values of this dictionary are sets of tuples. Each tuple in the set is
+    of the form (variable name, label) where the variable name is a string and
+    the label is an integer. Example:
+
+        1 -> set((x, 9))
+        2 -> set()
+        3 -> set((y, 2), (z, 7))
+
+    :param dict[int, set[(str, str)]] genSets: Gen set of the CFG.
     :rtype: dict[int, set[(str, int)]]
     """
-    # Get generators for the various attributes for convenience.
-    nodes = cfg.nodes()
-    labels = (node.name for node in nodes)
-    variables = (node.attr["var"] for node in nodes)
-    types = (node.attr["type"] for node in nodes)
 
-    # Generate the genSet. For non assignment nodes, the value is None.
-    genSet = dict()
-    for node, label, variable, type in zip(nodes, labels, variables, types):
-        genSet[label] = {(variable, label)} if type == NODE_ASSN else set()
+    killSets = dict()
 
-    return genSet
+    # We'll be iterating over the gen set instead of the CFG because all of the
+    # information we need to construct the kill set is contained in the gen set.
+    # The kill set for a basic block is the set of all other generations for the
+    # same variable, which is conveniently stored in the gen set.
+    for label, genSet in genSets.iteritems():
+        killSets[label] = set()
 
+        # A basic block's gen set must have at least 1 item to have a kill set.
+        # For each generation in a basic blocks gen set, retrieve the variable
+        # that is generated so that we can look for generations in other basic
+        # blocks' gen sets that modify the same variable.
+        # Reminder: g = (var, label), so g[0] = var
+        if genSet:
+            for generation in genSet:
+                variable = generation[0]
 
-def getKillSets(genSet):
-    killSet = dict()
-    for label, gen in genSet.iteritems():
-        killSet[label] = set()
-        if gen:
-            # variable = gen.pop()[0]
-            variable = next(iter(gen))[0]
-            for gs in genSet.values():
-                for v, l in gs:
-                    if v == variable and l != label:
-                        killSet[label] |= gs
+                # For each generation in each gen set (except generations with
+                # the same label as the gen set we are inspecting, as those are
+                # it's own generations!), for any generation that modifies the
+                # same variable as the outer gen set, add that generation to the
+                # kill set.
+                for gs in genSets.values():
+                    for v, l in gs:
+                        if v == variable and l != label:
+                            killSets[label] |= gs
 
-    return killSet
+    return killSets
 
 
 if __name__ == "__main__":
@@ -87,31 +122,31 @@ if __name__ == "__main__":
     g = AGraph(directed=True)
 
     # Nodes
-    g.add_node("start", label="START",              type=NODE_NONE,     var='')
-    g.add_node("n0",    label="x := 4;",            type=NODE_ASSN,     var='x')
-    g.add_node("n1",    label="y := 7;",            type=NODE_ASSN,     var='y')
-    g.add_node("n2",    label="while x < 10 do",    type=NODE_WHILE,    var='')
-    g.add_node("n3",    label="if y > 3 then",      type=NODE_IF,       var='')
-    g.add_node("n4",    label="y := x + 1;",        type=NODE_ASSN,     var='y')
-    g.add_node("n5",    label="x := x + 1;",        type=NODE_ASSN,     var='x')
-    g.add_node("n6",    label="a := x + 1;",        type=NODE_ASSN,     var='a')
-    g.add_node("n7",    label="y := x + 2;",        type=NODE_ASSN,     var='y')
-    g.add_node("n8",    label="x := x + 1;",        type=NODE_ASSN,     var='x')
-    g.add_node("end",   label="END",                type=NODE_NONE,     var='')
+    g.add_node("start", label="START",             type=NODE_NONE,     var='')
+    g.add_node("0",    label="x := 4;",            type=NODE_ASSN,     var='x')
+    g.add_node("1",    label="y := 7;",            type=NODE_ASSN,     var='y')
+    g.add_node("2",    label="while x < 10 do",    type=NODE_WHILE,    var='')
+    g.add_node("3",    label="if y > 3 then",      type=NODE_IF,       var='')
+    g.add_node("4",    label="y := x + 1;",        type=NODE_ASSN,     var='y')
+    g.add_node("5",    label="x := x + 1;",        type=NODE_ASSN,     var='x')
+    g.add_node("6",    label="a := x + 1;",        type=NODE_ASSN,     var='a')
+    g.add_node("7",    label="y := x + 2;",        type=NODE_ASSN,     var='y')
+    g.add_node("8",    label="x := x + 1;",        type=NODE_ASSN,     var='x')
+    g.add_node("end",   label="END",               type=NODE_NONE,     var='')
 
     # Edges
-    g.add_edge("start", "n0")
-    g.add_edge("n0", "n1")
-    g.add_edge("n1", "n2")
-    g.add_edge("n2", "n3")
-    g.add_edge("n2", "end")
-    g.add_edge("n3", "n4")
-    g.add_edge("n3", "n6")
-    g.add_edge("n4", "n5")
-    g.add_edge("n5", "n8")
-    g.add_edge("n6", "n7")
-    g.add_edge("n7", "n8")
-    g.add_edge("n8", "n2")
+    g.add_edge("start", "0")
+    g.add_edge("0", "1")
+    g.add_edge("1", "2")
+    g.add_edge("2", "3")
+    g.add_edge("2", "end")
+    g.add_edge("3", "4")
+    g.add_edge("3", "6")
+    g.add_edge("4", "5")
+    g.add_edge("5", "8")
+    g.add_edge("6", "7")
+    g.add_edge("7", "8")
+    g.add_edge("8", "2")
 
     # Output a diagram of the control-flow graph.
     g.draw("output/input2.png", format="png", prog="dot")
@@ -120,9 +155,13 @@ if __name__ == "__main__":
     killSets = getKillSets(genSets)
 
     print "GEN"
-    for label, gs in genSets.iteritems():
+    print "==="
+
+    for label, gs in sorted(genSets.iteritems()):
         print label, gs
 
+    print
     print "KILL"
-    for label, ks in killSets.iteritems():
+    print "===="
+    for label, ks in sorted(killSets.iteritems()):
         print label, ks
